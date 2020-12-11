c74d8bf47845a55fac16121131e3cb9b3a268244
Tyler Diminick <tdiminick@gmail.com>
2020-12-10 20:48:07 -0500

    adding day 2 input file and project1 python source code solution

---
+++ project1.py
@@ -0,0 +1,32 @@
+validPasswordCount = 0
+
+def count(pstring, char):
+	cnt = 0
+	for i in pstring:
+		if i == char:
+			cnt += 1 # cnt = cnt + 1
+
+	return cnt
+
+# read file and add each number to an array
+with open('password_input.txt') as f:
+	for line in f:
+		lineSplit = line.split(":");
+		password = lineSplit[1].strip()
+		
+		reqSplit = lineSplit[0].split()
+		reqChar = reqSplit[1]
+		
+		boundSplit = reqSplit[0].split("-")
+		lowerBound = int(boundSplit[0])
+		upperBound = int(boundSplit[1])
+
+		cnt = count(password, reqChar)
+
+		if cnt >= lowerBound and cnt <= upperBound:
+			validPasswordCount += 1
+
+	print(validPasswordCount)
+
+
+# [lower bound]-[upper bound] [reqChar]: [password]
